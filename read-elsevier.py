b80c8f98a9721b96226ee805277e1728c9c77260
Jared <jared.j.lorince@gmail.com>
2017-02-07 12:44:56 -0600

    new elsevier parsing done?

--- read-elsevier.py
+++ read-elsevier.py
@@ -1,16 +1,17 @@
 import gzip,codecs,time,datetime,glob,logging,string,os,pymssql
-from import tqdm as tq
+from tqdm import tqdm as tq
 from lxml import etree
 from nltk.tokenize import word_tokenize
 import multiprocessing as mp
+import numpy as np
 
 server,user,password = [line.strip() for line in open('server_credentials.txt')]
-ddir = "P:/Projects/WoS/wos-text-dynamics-data/raw/"
+ddir = "P:/Projects/WoS/wos-text-dynamics-data/elsevier/raw/"
 
 id_dict = {}
 for line in gzip.open('data/SD_WoS_id_match_DEDUPED.txt.gz'):
     el,wos = line.strip().split()
-    id_dict[int(el)] = wos
+    id_dict[int(el)] = wos.decode('utf8')
 
 
 def process(input_tuple):
@@ -21,12 +22,19 @@ def process(input_tuple):
     cursor = conn.cursor()
     cursor.execute("SELECT FileID, PaperContent FROM [Papers].[dbo].[Papers] where FileID>{} and FileID<={}".format(min_id,max_id))
 
-    with gzip.open("{}metadata_{}".format(ddir,idx),'w') as meta_out,gzip.open("{}text_{}".format(ddir,idx),'w') as text_out:
-        for FileID,PaperContent in cursor:
+    with gzip.open("{}metadata_{}".format(ddir,idx),'w') as meta_out,\
+         gzip.open("{}matched/text_{}".format(ddir,idx),'w') as matched_out,\
+         gzip.open("{}unmatched/text_{}".format(ddir,idx),'w') as unmatched_out:
+        for i,(FileID,PaperContent) in enumerate(cursor,1):
             rawtext,found_abstract,found_formatted_text,found_rawtext = parse_xml(PaperContent)
-            wos_id = id_dict[FileID]
-            meta_out.write("{}\t{}\t{}\t{}\t{}\n".format(FileID,wos_id,found_abstract,found_formatted_text,found_rawtext))
-            text_out.write("{}\t{}\n".format(wos_id,' '.join(rawtext)))
+            try:
+                wos_id = id_dict[FileID]
+                meta_out.write("{}\t{}\t{}\t{}\t{}\n".format(FileID,wos_id,found_abstract,found_formatted_text,found_rawtext).encode('utf8'))
+                matched_out.write("{}\t{}\n".format(wos_id,' '.join(rawtext)).encode('utf8'))
+            except KeyError:
+                unmatched_out.write("{}\t{}\n".format(FileID,' '.join(rawtext)).encode('utf8'))
+            if i%1000==0:
+                print("Idx {}: {} records processed".format(idx,i))
     cursor.close()
     conn.close()
 
@@ -83,9 +91,18 @@ def parse_xml(text):
 
 if __name__=='__main__':
 
-    procs = np.cpu_count()
+    procs = mp.cpu_count()
 
-    chunks = np.percentile(id_dict.keys(),np.linspace(0,100,procs))
+    conn = pymssql.connect(server, user, password, "tempdb")
+    cursor = conn.cursor()
+    cursor.execute("SELECT FileID from [Papers].[dbo].[Papers]")
+    all_ids = sorted([fid[0] for fid in cursor.fetchall()])
+    cursor.close()
+    conn.close()
+
+    chunks = np.percentile(all_ids,np.linspace(0,100,procs)).astype(int)
     chunks[0]=-1
+    #np.array_split(np.array(sorted([k for k in id_dict.keys()])),60)
+    #
 
     pool.map(process,zip(range(procs),chunks,chunks[1:]))
