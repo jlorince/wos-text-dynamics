b792843f879acc62ba7e8f31efa6b1e33c2ab558
Jared <jared.j.lorince@gmail.com>
2017-02-08 09:43:21 -0600

    more debugging of read-elsevier

--- read-elsevier.py
+++ read-elsevier.py
@@ -13,30 +13,47 @@ for line in gzip.open('data/SD_WoS_id_match_DEDUPED.txt.gz'):
     el,wos = line.strip().split()
     id_dict[int(el)] = wos.decode('utf8')
 
+import time,datetime
+class timed(object):
+    def __init__(self,desc='command',pad='',**kwargs):
+        self.desc = desc
+        self.kwargs = kwargs
+        self.pad = pad
+    def __enter__(self):
+        self.start = time.time()
+        print('{} started...'.format(self.desc))
+    def __exit__(self, type, value, traceback):
+        if len(self.kwargs)==0:
+            print('{}{} complete in {}{}'.format(self.pad,self.desc,str(datetime.timedelta(seconds=time.time()-self.start)),self.pad))
+        else:
+            print('{}{} complete in {} ({}){}'.format(self.pad,self.desc,str(datetime.timedelta(seconds=time.time()-self.start)),','.join(['{}={}'.format(*kw) for kw in self.kwargs.iteritems()]),self.pad))
+
 
 def process(input_tuple):
-    idx,min_id,max_id = input_tuple
+    idx,min_id,max_id,total = input_tuple
 
     # read data
     conn = pymssql.connect(server, user, password, "tempdb")
     cursor = conn.cursor()
     cursor.execute("SELECT FileID, PaperContent FROM [Papers].[dbo].[Papers] where FileID>{} and FileID<={}".format(min_id,max_id))
 
-    with gzip.open("{}metadata_{}".format(ddir,idx),'w') as meta_out,\
-         gzip.open("{}matched/text_{}".format(ddir,idx),'w') as matched_out,\
-         gzip.open("{}unmatched/text_{}".format(ddir,idx),'w') as unmatched_out:
-        for i,(FileID,PaperContent) in enumerate(cursor,1):
-            rawtext,found_abstract,found_formatted_text,found_rawtext = parse_xml(PaperContent)
-            try:
-                wos_id = id_dict[FileID]
-                meta_out.write("{}\t{}\t{}\t{}\t{}\n".format(FileID,wos_id,found_abstract,found_formatted_text,found_rawtext).encode('utf8'))
-                matched_out.write("{}\t{}\n".format(wos_id,' '.join(rawtext)).encode('utf8'))
-            except KeyError:
-                unmatched_out.write("{}\t{}\n".format(FileID,' '.join(rawtext)).encode('utf8'))
-            if i%1000==0:
-                print("Idx {}: {} records processed".format(idx,i))
-    cursor.close()
-    conn.close()
+    with timed('Processing idx {}'.format(idx)):
+        with gzip.open("{}metadata_{}".format(ddir,idx),'w') as meta_out,\
+             gzip.open("{}matched/text_{}".format(ddir,idx),'w') as matched_out,\
+             gzip.open("{}unmatched/text_{}".format(ddir,idx),'w') as unmatched_out:
+            for i,(FileID,PaperContent) in enumerate(cursor,1):
+                rawtext,found_abstract,found_formatted_text,found_rawtext = parse_xml(PaperContent)
+                wos_id = id_dict.get(FileID,'')
+                if wos_id is not '':
+                    matched_out.write("{}\t{}\n".format(wos_id,' '.join(rawtext)).encode('utf8'))
+                else:
+                    unmatched_out.write("{}\t{}\n".format(FileID,' '.join(rawtext)).encode('utf8'))
+                meta_out.write("{}\t{}\t{}\t{}\t{}\n".format(FileID,wos_id,int(found_abstract),int(found_formatted_text),int(found_rawtext)).encode('utf8'))
+                
+                if i%1000==0:
+                    print("Idx {}: {}/{} ({:2f}%) records processed".format(idx,i,total,100*(i/total)))
+        cursor.close()
+        conn.close()
 
 
 def parse_xml(text):
@@ -91,7 +108,7 @@ def parse_xml(text):
 
 if __name__=='__main__':
 
-    procs = 30 #mp.cpu_count()
+    procs = 60 #mp.cpu_count()
     pool = mp.Pool(procs)
 
     conn = pymssql.connect(server, user, password, "tempdb")
@@ -104,6 +121,6 @@ if __name__=='__main__':
     chunks = np.percentile(all_ids,np.linspace(0,100,procs)).astype(int)
     chunks[0]=-1
     #np.array_split(np.array(sorted([k for k in id_dict.keys()])),60)
-    #
+    per_chunk_counts = pd.Series(np.digitize(all_ids,chunks,right=True)-1).value_counts().sort_index().values
 
-    pool.map(process,zip(range(procs),chunks,chunks[1:]))
+    pool.map(process,zip(range(procs),chunks,chunks[1:],per_chunk_counts))
